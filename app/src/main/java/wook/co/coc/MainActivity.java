9ed78198c7135085799679cb66b1c553e373978f
hankgood95 <hankgood958@gmail.com>
2021-07-13 00:43:29 +0900

    MVVM 패턴으로 리사이클러 뷰 완성

--- app/src/main/java/wook/co/coc/MainActivity.java
+++ app/src/main/java/wook/co/coc/MainActivity.java
@@ -1,22 +1,36 @@
 package wook.co.coc;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.LinearLayout;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import wook.co.coc.adapters.RecyclerViewAdapter;
+import wook.co.coc.models.NicePlace;
+import wook.co.coc.viewmodels.MainActivityViewModel;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
     //vars
-    private ArrayList<String> mNames = new ArrayList<>();
-    private ArrayList<String> mImageUrls = new ArrayList<>();
+    private FloatingActionButton mFab;
+    private RecyclerView mRecyclerView;
+    private RecyclerViewAdapter mAdapter;
+    private ProgressBar mProgressBar;
+    private MainActivityViewModel mMainActivityViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,53 +38,58 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate: started.");
 
-        initImageBitmaps(); //이미지를 가져올 사이크를 ArrayList에 추가
+        mFab = findViewById(R.id.fab);
+        mRecyclerView = findViewById(R.id.recycler_view);
+        mProgressBar = findViewById(R.id.progress_bar);
+
+        mMainActivityViewModel = ViewModelProviders.of(this).get(MainActivityViewModel.class);
+
+        mMainActivityViewModel.init();
+
+        mMainActivityViewModel.getNicePlaces().observe(this, new Observer<List<NicePlace>>() {
+            @Override
+            public void onChanged(List<NicePlace> nicePlaces) {
+                mAdapter.notifyDataSetChanged();
+            }
+        });
+
+        mMainActivityViewModel.getIsUpdating().observe(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean aBoolean) {
+                if(aBoolean){
+                    showProgressBar();
+                }else{
+                    hideProgressBar();
+                    mRecyclerView.smoothScrollToPosition(mMainActivityViewModel.getNicePlaces().getValue().size()-1);
+                }
+            }
+        });
+
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mMainActivityViewModel.addNewValue(new NicePlace(
+                        "https://i.imgur.com/ZcLLrkY.jpg",
+                        "Washington"
+                ));
+            }
+        });
+
+        initRecyclerView(); //위에서 가져온 ImageList를 리사이클 뷰에 집어 넣는다.
     }
 
-    private void initImageBitmaps(){
-        Log.d(TAG, "initImageBitmaps: preparing bitmaps.");
+    private void hideProgressBar() {
+        mProgressBar.setVisibility(View.GONE);
+    }
 
-        mImageUrls.add("https://i.redd.it/tpsnoz5bzo501.jpg");
-        mNames.add("Trondheim");
-
-        mImageUrls.add("https://i.redd.it/qn7f9oqu7o501.jpg");
-        mNames.add("Portugal");
-
-        mImageUrls.add("https://i.redd.it/j6myfqglup501.jpg");
-        mNames.add("Rocky Mountain National Park");
-
-
-        mImageUrls.add("https://i.redd.it/0h2gm1ix6p501.jpg");
-        mNames.add("Mahahual");
-
-        mImageUrls.add("https://i.redd.it/k98uzl68eh501.jpg");
-        mNames.add("Frozen Lake");
-
-
-        mImageUrls.add("https://i.redd.it/glin0nwndo501.jpg");
-        mNames.add("White Sands Desert");
-
-        mImageUrls.add("https://i.redd.it/obx4zydshg601.jpg");
-        mNames.add("Austrailia");
-
-        mImageUrls.add("https://i.imgur.com/ZcLLrkY.jpg");
-        mNames.add("Washington");
-
-        initRecyclerView();
+    private void showProgressBar() {
+        mProgressBar.setVisibility(View.VISIBLE);
     }
 
     private void initRecyclerView(){
-        Log.d(TAG, "initRecyclerView: init recyclerview.");
-        RecyclerView recyclerView = findViewById(R.id.recycler_view); //Recycler View를 만든것 xml id로 받아옴
-        RecyclerViewAdapter adapter = new RecyclerViewAdapter(mNames, mImageUrls,this); //해당 ArrayList와 Content로 Adapter class 생성
-        recyclerView.setAdapter(adapter); //recyclerView는 setAdapter를 통해서 adapter를 받아야 한다.
-        //recycler view가 화면에 표시될때 아이템 뷰들이 리사이클러 내부에서 배치되는 형태를 관리하는 요소가 LinearLayoutManager이다.
-        //따라서 수직수평을 나타낼수 있는 LinearLayoutManager, 바둑판 모양의 GridLayoytManager, 엇갈림 격자 형태인 StaggerdedGridLayoutManager등이 있다.
-        //아래 소스와 같은 경우엔 LinearLayoutManager이니 수평 혹은 수직이 된다.
-        LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
-        //mLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
-        mLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-        recyclerView.setLayoutManager(mLayoutManager);
-
+        mAdapter = new RecyclerViewAdapter(mMainActivityViewModel.getNicePlaces().getValue(),this);
+        RecyclerView.LayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
---
+++ app/src/main/java/wook/co/coc/models/NicePlace.java
@@ -0,0 +1,35 @@
+package wook.co.coc.models;
+
+public class NicePlace {
+
+    private String title; //사진 제목을 담을 변수
+    private String imageUrl; // imageurl을 담을 변수
+
+    public NicePlace() {}
+
+    public NicePlace(String imageUrl,String title) {
+        this.title = title;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "NicePlace{" + "title='" + title + '\'' + ", imageUrl='" + imageUrl + '\'' + '}';
+    }
+}
